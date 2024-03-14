aa40eb1764ba30f0f2e2910d8ba53cd708875e10
kwspp <71198619+kwspp@users.noreply.github.com>
2024-03-14 14:50:20 -0400

    Update visuals of convo input

--- senti-health/src/components/PatientConvo/PatientConvo.js
+++ senti-health/src/components/PatientConvo/PatientConvo.js
@@ -3,13 +3,14 @@ import './PatientConvo.css';
 
 const PatientConvo = () => {
   return (
-    <div className="patient-convo">
-        <p className="convo-title">Patient Conversation</p>
-            <div className="convo-input">
-                <input type="text" placeholder="Type here..." />
-            </div>
-        <p>Enter details to save in the conversation</p>
-    </div>
+      <div className="patient-convo">
+          <p className="convo-title">Patient Conversation</p>
+              <div className="convo-input">
+                  <textarea placeholder="Type here..." />
+              </div>
+          <p>Enter details to save in the conversation</p>
+        <button type="submit">Submit</button>
+      </div>
   )
 }
 
