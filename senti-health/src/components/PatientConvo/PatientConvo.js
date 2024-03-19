2c8c143f397eefdc83b4f0463c713f48994809ac
kwspp <71198619+kwspp@users.noreply.github.com>
2024-03-19 00:30:57 -0400

    Added some comments

--- senti-health/src/components/PatientConvo/PatientConvo.js
+++ senti-health/src/components/PatientConvo/PatientConvo.js
@@ -5,18 +5,22 @@ import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognitio
 
 
 const PatientConvo = ({ selectedPatient  }) => {
+  //  state management for user input or voice recording
   const [userInput, setUserInput] = useState("");
   const [isRecording, setIsRecording] = useState(false);
+  // const for speech recognition
   const {
     transcript,
     resetTranscript,
     browserSupportsSpeechRecognition
   } = useSpeechRecognition();
 
+  // handles updating the user input for the conversation
   const handleUserInput = (event) => {
     setUserInput(event.target.value);
   };
 
+  // toggles recording on/off, displays correct button based off state
   const toggleRecording = () => {
     if (isRecording) {
       SpeechRecognition.stopListening();
@@ -32,14 +36,17 @@ const PatientConvo = ({ selectedPatient  }) => {
     return <span>Browser doesn't support speech recognition.</span>;
   }
 
+  // handles sentiment analysis when conversation is submitted
   const submitSentimentAnalysis = async () => {
     if (selectedPatient == null) {
       window.alert("Must select a patient");
       return null
     }
+    // on submit, clear the field after saving the text into var
     const text = userInput || transcript;
     setUserInput('');
     resetTranscript();
+    // pass text into spaCytextblob python flask API
     try {
       const response = await fetch('http://localhost:5000/analyze_sentiment', {
         method: 'POST',
@@ -50,6 +57,7 @@ const PatientConvo = ({ selectedPatient  }) => {
       });
       const data = await response.json();
       console.log(`Sentiment score: ${data.sentiment}, Selected Patient ID: ${selectedPatient}`);
+      // save sentiment score to patient
       addSentimentScore(selectedPatient, data.sentiment)
     } catch (error) {
       console.error('Error:', error);
@@ -58,6 +66,7 @@ const PatientConvo = ({ selectedPatient  }) => {
 
   const addSentimentScore = async (selectedPatient, sentiment) => {
     try {
+      // fetch current sentiment scores from patient then add new value and update in backend
       let sentimentScores = await fetchSentimentScores(selectedPatient);
       sentimentScores.push(sentiment);      
       await updateSentimentScores(selectedPatient, sentimentScores);
