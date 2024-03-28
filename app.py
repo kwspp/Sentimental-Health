bc9bbff165b5c65f59d218a9f3ae68979fdca1c4
kwspp <71198619+kwspp@users.noreply.github.com>
2024-03-27 23:17:22 -0400

    updated to add home screen to flask app

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template_string
 import spacy
 from spacytextblob.spacytextblob import SpacyTextBlob
 from flask_cors import CORS
@@ -9,6 +9,11 @@ CORS(app)
 # Load spaCy model and add the spaCyTextBlob component to the pipeline
 nlp = spacy.load('en_core_web_sm')
 nlp.add_pipe('spacytextblob')
+
+@app.route('/')
+def home():
+    # Display a simple message confirming the app is running
+    return '<h1>The Flask App is Running!</h1>'
 
 @app.route('/analyze_sentiment', methods=['POST'])
 def analyze_sentiment():
